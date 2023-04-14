a4342d040c6061b6aec9798dceea33abe8dea49a
Joowon Yang <jywleah1101@berkeley.edu>
2023-04-14 16:50:13 +0000

    test_utils

--- ligotools/test/test_utils.py
+++ ligotools/test/test_utils.py
@@ -1,4 +1,3 @@
-# Standard python numerical analysis imports:
 import numpy as np
 from scipy import signal
 from scipy.interpolate import interp1d
@@ -14,8 +13,10 @@ from ligotools import readligo as rl
 
 # import functions from ligotools.utils
 from ligotools import utils
-
 from scipy.io import wavfile
+
+fnjson = "data/BBH_events_v3.json"
+events = json.load(open(fnjson,"r"))
 
 eventname = ''
 eventname = 'GW150914' 
@@ -28,11 +29,6 @@ make_plots = 1
 plottype = "png"
 #plottype = "pdf"
 
-# Read the event properties from a local json file
-fnjson = "data/BBH_events_v3.json"
-events = json.load(open(fnjson,"r"))
-
-# Extract the parameters for the desired event:
 event = events[eventname]
 fn_H1 = event['fn_H1']             
 fn_L1 = event['fn_L1']              
@@ -41,14 +37,8 @@ fs = event['fs']
 tevent = event['tevent']           
 fband = event['fband']
 
-# read in data from H1 and L1, if available:
 strain_H1, time_H1, chan_dict_H1 = rl.loaddata("data/"+fn_H1, 'H1')
 strain_L1, time_L1, chan_dict_L1 = rl.loaddata("data/"+fn_L1, 'L1')
-
-# both H1 and L1 will have the same time vector, so:
-time = time_H1
-# the time sample interval (uniformly sampled!)
-dt = time[1] - time[0]
 
 NFFT = 4*fs
 Pxx_H1, freqs = mlab.psd(strain_H1, Fs = fs, NFFT = NFFT)
@@ -57,7 +47,9 @@ Pxx_L1, freqs = mlab.psd(strain_L1, Fs = fs, NFFT = NFFT)
 psd_H1 = interp1d(freqs, Pxx_H1)
 psd_L1 = interp1d(freqs, Pxx_L1)
 
-# Test 1
+time = time_H1
+dt = time[1] - time[0]
+
 def test_whiten():
     strain = np.random.rand(1000)
     dt = 0.1
@@ -66,14 +58,10 @@ def test_whiten():
     white_ht = utils.whiten(strain_H1, psd_H1, dt)
     assert len(white_ht) == len(strain_H1)
 
-# Test 2
 def test_write_wavfile():
     # Create some test data
     fs = 44100
-    duration = 5.0
-    f = 440.0
-    t = np.linspace(0, duration, int(fs * duration), endpoint=False)
-    data = 0.5 * np.sin(2 * np.pi * f * t)
+    data = np.random.rand(1000)
 
     # Call the function with the test data
     filename = "test.wav"
@@ -86,7 +74,6 @@ def test_write_wavfile():
     # Clean up the written file
     os.remove(filename)
 
-# Test 3
 def test_reqshift():
     # Create a test signal
     fs = 4096
@@ -104,25 +91,24 @@ def test_reqshift():
     expected_data = np.sin(2*np.pi*f2*t2)
     assert np.allclose(shifted_data, expected_data, rtol=1e-3)
     
-# Test 4
 def test_make_plot():
     # create some test data
     det = 'H1'
-    strain_H1_whitenbp = np.random.randn(4096)
-    strain_L1_whitenbp = np.random.randn(4096)
-    template_match = np.random.randn(4096)
-    time = np.linspace(-1, 1, 4096)
-    timemax = 0.0
-    SNR = np.abs(np.random.randn(4096))
+    strain_H1_whitenbp = np.random.randn(1000)
+    strain_L1_whitenbp = np.random.randn(1000)
+    template_match = np.random.randn(1000)
+    time = np.linspace(-1, 1, 1000)
+    timemax = 0.5
+    SNR = np.abs(np.random.randn(1000))
     eventname = 'test_event'
     plottype = 'png'
-    tevent = 0.0
+    tevent = 0.2
     template_fft = np.fft.rfft(template_match)
-    datafreq = np.fft.rfftfreq(len(strain_H1_whitenbp), d=1.0/4096)
+    datafreq = np.fft.rfftfreq(len(strain_H1_whitenbp), d=1.0/1000)
     d_eff = 1.0
-    freqs = datafreq
+    freqs = datafreq+1
     data_psd = np.abs(np.random.randn(len(datafreq)))**2
-    fs = 4096
+    fs = 1000
     
     # test the function
     utils.make_plot(det, strain_H1_whitenbp, strain_L1_whitenbp, template_match, time, timemax, SNR, eventname, plottype, tevent, template_fft, datafreq, d_eff, freqs, data_psd, fs)
